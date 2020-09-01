657e4628c0c721222c83f4621eceff67a7a77c21
John Doe <johndoe@MacBookPro.local>
2020-09-01 15:21:30 +0200

    Calculating module finished

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
  let btn = document.querySelector('form #addbtn');
  let form = document.querySelector('form');
  let summary = document.getElementsByClassName('price');
-
- let validate = true
- let formIndex = 0;
+ 
  let price = 0;
 
-//Calculate price
+//Price calculation
 const calcPrice = (width, height, quantity) => {
-    price += (width*height*quantity*350);
+    return ((Math.floor((width*height*quantity*350)/100)+1)*100);
+}
+const showPrice = (val, add) => {
+    add ? price += val : price -= val;
     summary[0].textContent = price + 'zł';
 }
-
-const addEvList = () => {
-    document.querySelectorAll('#erase').forEach(element => {
-        element.addEventListener('click', () => {
-            element.parentElement.remove();
-        })
+//Create erase button
+const addEraseBtn = (element) => {
+    element.id = 'erase';
+    element.classList += ' erase';
+    element.addEventListener('click', () => {
+        showPrice(element.parentNode.id, false)
+        element.parentNode.remove();
     })
+}
+//Form validation
+const validateForm = (e) => {
+    let data = e.target.parentElement;
+    if(data[0].value !== '' && data[1].value != '' && data[2].value != '') return false;
+    // return false;
+    return true;
+    //to stop return true 
 }
 
 
 btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    [...form].forEach(element => element.value == "" ? validate = false : validate = true)
-    if(validate) return;
-    calcPrice(form[0].value, form[1].value, form[2].value);
+    //e.preventDefault();
+    //console.log(e.target.parentElement);
+    if(validateForm(e)) return;
+    let itemPrice = calcPrice(form[0].value, form[1].value, form[2].value);
+    showPrice(itemPrice, true);
     let newForm = form.cloneNode(true);
-    newForm.id = ++formIndex;
-    //newForm[3].addEventListener('click', eraseElement(formIndex));
-    newForm[3].id = "erase"
-    newForm[3].textContent = "Usuń";
+    newForm.id = itemPrice;
+    addEraseBtn(newForm[3])
     document.getElementById('elementList').appendChild(newForm);
     [...form].forEach(element => element.value = '');
-    addEvList();
 })
