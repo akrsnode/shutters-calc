b3025dcb38fb623ca8729a2cc8f49d45e0cffa7c
vansmoe <61562397+vansmoe@users.noreply.github.com>
2021-03-18 15:48:00 +0100

    Wrap inputs into form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
  let btn = document.querySelector('form #addbtn');
  let helpBtns = [...document.querySelectorAll('#helpbtn, #closebtn')];
- let form = document.querySelector('form');
+ const submitFormBtns = document.querySelectorAll(".order"); 
+ let formItem = document.querySelector('form > div');
  let summary = document.getElementsByClassName('price');
+
  
  let price = 0;
 
@@ -23,46 +25,73 @@ const addEraseBtn = (element) => {
         element.parentNode.remove();
     })
 }
-//Form validation
+//Form validation, return true when incorrect
 const validateForm = (e) => {
-    let data = e.target.parentElement;
-    for(let i = 0; i < 3; i++) {
-        if(data[i].value <= 0) {
-            form[i].classList.add('errorborder');
+    let data = e.target.parentElement.children;
+    for(let i = 0; i < 4; i++) {
+        if(data[i].value <= 0 && i != 1) {
+            data[i].classList.add('errorborder');
         } else {
-            form[i].classList.remove('errorborder');
+            data[i].classList.remove('errorborder');
         }
     }
-    if(data[0].value > 0 && data[1].value > 0 && data[2].value > 0) return false;
+    if(data[0].value > 0 && data[2].value > 0 && data[3].value > 0) return false;
     return true;
 }
+
+
 //Change element
-const changeElement = (e) => {
-    let parent = e.target.parentElement;
+const updateElement = (e) => {
+    if (validateForm(e)) return;
+
+    const parent = e.target.parentElement;
+    const values = [parent.children[0].value, parent.children[2].value, Math.floor(parent.children[3].value)];
+
     showPrice(parent.id, false)
-    let itemPrice = calcPrice(parent[0].value, parent[1].value, Math.floor(parent[2].value));
-    parent.id = itemPrice;
-    showPrice(itemPrice, true);    
+    let newPrice = calcPrice(...values);
+    parent.id = newPrice;
+    showPrice(newPrice, true);    
 }
+
+
 //Floor quantity value
 const floorQuantity = (e) => {
     e.target.value = Math.floor(e.target.value);
 }
 
+//Event listeners
+
 btn.addEventListener('click', (e) => {
+
     if(validateForm(e)) return;
-    let itemPrice = calcPrice(form[0].value, form[1].value, form[2].value);
+
+    const values = [formItem.children[0].value, formItem.children[2].value, formItem.children[3].value];
+
+    const itemPrice = calcPrice(...values);
     showPrice(itemPrice, true);
-    let newForm = form.cloneNode(true);
+
+    let newForm = formItem.cloneNode(true);
     newForm.id = itemPrice;
-    newForm[0].tabIndex = '-1';
-    addEraseBtn(newForm[3]);
+    addEraseBtn(newForm.children[4]);
+
     document.getElementById('elementList').appendChild(newForm);
-    [...form].forEach(element => element.value = '');
+
+    [...formItem.children].forEach(element => element.value = '');
     let inputs = [...document.querySelectorAll('input')];
-    inputs.slice(3).forEach(element => element.addEventListener('change', changeElement))
+    inputs.slice(3).forEach(element => element.addEventListener('change', updateElement))
 })
 
 helpBtns.forEach(element => element.addEventListener('click', () => {
     document.getElementById('help').classList.toggle('nodisplay');
 }))
+
+submitFormBtns.forEach(e => e.addEventListener("click", () => {
+    const inputs = document.querySelectorAll("form > div > input");
+
+    if (inputs.length == 3) return alert("Aby złożyć zamówienie dodaj min. 1 front.");
+    for (let i = 3; i < inputs.length; i++) {
+        if (inputs[i].value <= 0) return alert(`Nieprawidłowa wartość frontu!`);
+    }
+
+    document.querySelector("form").submit();
+}));
